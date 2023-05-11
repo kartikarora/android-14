506fb69e6af8d678638b45d79bc1a066496d4254
Kartik Arora <kartik.arora1995@gmail.com>
2023-05-11 21:22:29 +1000

    update build tools

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -6,5 +6,5 @@ object AndroidConfig {
     const val VersionName = "0.2.1"
     const val Namespace = "me.kartikarora.android14"
     const val KotlinCompilerExtensionVersion = "1.4.1"
-    const val BuildTools = "34.0.0 rc3"
+    const val BuildTools = "34.0.0 rc4"
 }
